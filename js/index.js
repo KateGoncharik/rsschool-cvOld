a6353089ab762f54983f736f2273cf4491b7fb45
KateGoncharik <113840749+KateGoncharik@users.noreply.github.com>
2023-02-28 13:51:50 +0300

    made burger-menu

--- js/index.js
+++ js/index.js
@@ -13,19 +13,19 @@ avatar.addEventListener('click', () => alert("Yes, it's me"));
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector("#hamburger");
-const close = document.querySelector("#close");
-const open = document.querySelector("#open");
+const closeIcon = document.querySelector("#close");
+const openIcon = document.querySelector("#open");
 
 
 function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-      menu.classList.remove("showMenu");
-      close.style.display = "none";
-      menu.style.display = "block";
+    if (menu.classList.contains("show-menu")) {
+      menu.classList.remove("show-menu");
+      closeIcon.style.display = "none";
+      openIcon.style.display = "block";
     } else {
-      menu.classList.add("showMenu");
-      close.style.display = "block";
-      menu.style.display = "none";
+      menu.classList.add("show-menu");
+      closeIcon.style.display = "block";
+      openIcon.style.display = "none";
     }
   }
   
